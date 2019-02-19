ca0b98503af524e0da72341a3fd6d4f3e1089f19
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2019-02-19 14:00:48 +0300

    Changing the footer

--- footer.php
+++ footer.php
@@ -12,65 +12,69 @@
 ?>
 
 	</div><!-- #content -->
-	<footer class="footer-distributed">
-		<div class="footer-left">
-			<div class="co-logo">
-				<?php the_custom_logo(); ?>
-			</div>
-			<div class="footer-links">
-				<?php
-					wp_nav_menu([
-						'theme_location'	=> 'secondary',
-					]);
-				?>
-			</div>
-			<div class="footer-links">
-				<?php
-					wp_nav_menu([
-						'theme_location'	=> 'services',
-					]);
-				?>
-			</div>
-			<p>
-				Camp Stephen Group &copy; <?php echo date('Y') ?>
-			</p>
-		</div>
-		<div class="footer-center">
-			<div>
-				<i class="fa fa-map-marker"></i>
-				<p>
-					<span>5th Floor Royal Square,</span>Ngong Road Nairobi, Kenya
-				</p>
-			</div>
-			<div>
-				<i class="fa fa-phone"></i>
-				<p>+254 712 345 678</p>
-			</div>
-			<div>
-				<i class="fa fa-envelope"></i>
-				<p>
-					<a href="mailto:info@campstephen.com">info@campstephen.com</a>
-				</p>
-			</div>
-		</div>
-		<div class="footer-right">
-			<h4>About Camp Stephen Group</h4>
-			<p class="footer-company-about">				
-			Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
-			</p>
-			<p class="footer-company-about">Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.</p>
-			<p class="footer-company-about">Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.</p>
-			<div class="footer-icons">
-				<a href="#"><i class="fa fa-facebook"></i></a>
-				<a href="#"><i class="fa fa-twitter"></i></a>
-				<a href="#"><i class="fa fa-linkedin"></i></a>
-				<a href="#"><i class="fa fa-youtube"></i></a>
-			</div>
-		</div>
-	</footer>
+	<footer class="container-fluid">
+        <div class="footer-content">
+            <div class="row center-xs">
+                <div class="first-row">
+                    <h5>
+                        <?php the_custom_logo(); ?>
+                    </h5>
+                    <p><?php echo(get_bloginfo('description')); ?></p>
+                </div>
+            </div>
+            <div class="row second-row">
+                <div class="col-md-4">
+                    <h3>
+                        Contacts
+                    </h3>
+                    <div class="co-info">
+                        <p>
+                            Office:<a href="#"> Fifth Floor Royal Square</a><br />
+                            Ngong Road, Nairobi Kenya. 
+                        </p>
+                        <p>
+                            +254 712 456 789<br />
+                            info@campstephen.com
+                        </p>
+                    </div><!-- .co-info -->
+                </div><!-- .col-md-4 -->
+                <div class="col-md-4">
+                    <div class="co-links">
+                        <h3>Company</h3>
+                        <div class="footer-nav">
+                            <?php
+                                wp_nav_menu([
+                                    'menu'      =>          'primary',
+                                ]);
+                            ?>
+                        </div>
+                    </div><!-- .co-links -->
+                </div><!-- .col-md-4 -->
+                <div class="col-md-4">
+                    <div class="co-links">
+                            <h3>Services</h3>
+                            <div class="footer-nav">
+                                <?php
+                                    wp_nav_menu([
+                                        'menu'      =>      'services',
+                                    ]);
+                                ?>
+                            </div>
+                    </div><!-- .co-links -->
+                </div><!-- .col-md-4 -->
+            </div><!-- .row -->
+            <div class="row center-xs">
+                <div class="bottom-section">
+                    <p>
+                        <?php echo(date('Y')); ?>
+                        &copy; <a href="<?php echo(get_bloginfo('url')); ?>"><?php echo(get_bloginfo('site_name'));?></a>
+                    </p>
+                    <p><span class="theme_name">camper</span> Theme Developed by <a href="http://mwororokevin.com" target="_blank">Mwororo Kevin</a></p>
+                </div><!-- .bottom-section -->
+            </div><!-- .row center-xs -->
+        </div><!-- .footer-container -->
+    </footer><!-- .container-fluid -->
 </div><!-- #page -->
-
 <?php wp_footer(); ?>
-
 </body>
 </html>
